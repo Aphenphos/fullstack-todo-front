8eab8f81d3cc4f25f55ff2ab7d25f62ac80c645a
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-23 13:44:24 -0700

    added index and todo-utils for get/create todos

---
+++ todo-utils.js
@@ -0,0 +1,28 @@
+const BASE_URL = 'http://localhost:7890';
+//localhost:7890/api/v1/users
+
+export async function getAllTodos() {
+    const resp = await fetch(`${BASE_URL}/api/v1/todos`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
+        credentials: 'include',
+    });
+    return resp.json();
+}
+
+export async function createTodo(todo) {
+    const resp = await fetch(`${BASE_URL}/api/v1/todos`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+        credentials: 'include',
+    });
+    return resp.json();
+}
--- todo/todo.js
+++ todo/todo.js
@@ -1,7 +1,11 @@
-import { logoutUser } from "../utils.js";
+import { logoutUser, getUser } from "../utils.js";
+import { getAllTodos, createTodo } from '../todo-utils.js';
+
+let todos = [];
 
 const logOutButton = document.getElementById('logout');
 
 logout.addEventListener('click', async () => {
     await logoutUser();
 })
+
