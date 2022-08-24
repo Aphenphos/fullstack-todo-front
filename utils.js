4edac328c06716cd3a13206c7a6a5393c05934a1
MaxW <97769782+Aphenphos@users.noreply.github.com>
2022-08-24 15:44:52 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:7890';
+const BASE_URL = 'https://fullstack-todo-maxzak.herokuapp.com';
 //localhost:7890/api/v1/users
 export async function signUpUser(userInfo) {
     const resp = await fetch(`${BASE_URL}/api/v1/users`, {
